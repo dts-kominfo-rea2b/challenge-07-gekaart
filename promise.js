7533782073ab0ba8edee4a5139deba85ed30e078
gekaart <gekaarissta@gmail.com>
2022-06-10 21:30:05 +0700

    answer challenge 7

--- promise.js
+++ promise.js
@@ -1,7 +1,34 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi)=>{
+  let total = 0;
+  return new Promise((resolve, reject)=>{
+    if(promiseTheaterIXX == "" || promiseTheaterVGC == ""){
+      reject("data not found!")
+    }else{
+      promiseTheaterIXX()
+      .then((hasilSetelahMenonton)=>{
+        hasilSetelahMenonton.forEach((e) => {
+          if(e.hasil === emosi){
+            total += 1;
+          }
+        });
+      })
+      promiseTheaterVGC()
+      .then((hasilSetelahMenonton)=>{
+        hasilSetelahMenonton.forEach((e)=>{
+          if(e.hasil === emosi){
+            total +=1;
+          }
+        })
+        resolve(total)
+      }
+
+      )
+    }
+  })
+}
 
 module.exports = {
   promiseOutput,
